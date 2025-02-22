86bb179f803c54eab7e38803c6eb3be0d7e61229
sarrtle <sarrtle.dev@proton.me>
2025-02-23 01:44:20 +0800

    update exception and pyproject

--- fbtools/official/exceptions.py
+++ fbtools/official/exceptions.py
@@ -25,18 +25,33 @@ class UnknownValidationError(Exception):
         super().__init__(message)
 
 
+class GenerateUserAccessTokenError(Exception):
+    """Raise if something went wrong during generation of user access token."""
+
+    def __init__(self, message: str):
+        """Initialize GenerateUserAccessTokenError."""
+        super().__init__(message)
+
+
 class LoginError(Exception):
     """Raise if something went wrong during login."""
 
     def __init__(
         self,
-        error_type: UserValidationError | PageValidationError | UnknownValidationError,
+        error_type: (
+            UserValidationError
+            | PageValidationError
+            | UnknownValidationError
+            | GenerateUserAccessTokenError
+        ),
     ):
         """Initialize LoginError."""
         if isinstance(error_type, UserValidationError):
             message = "Error during login: Can't validate user token."
         elif isinstance(error_type, PageValidationError):
             message = "Error during login: Can't validate page token."
+        elif isinstance(error_type, GenerateUserAccessTokenError):
+            message = "Error during login: Can't generate user token."
         else:
             message = "Error during login: Unknown error."
         super().__init__(message)
